aa3807e38e26b0aa0fd829d42ee4bdf688c8cde3
JianKai <tony14256@gmail.com>
2020-08-27 21:40:06 +0800

    animate quick sort

--- src/lib/quick.js
+++ src/lib/quick.js
@@ -1,9 +1,9 @@
 function quickSort(data) {
   console.log(data);
-  sort(data, 1);
+  sort(data, 1, 0);
 }
 
-const sort = (data, time) => {
+const sort = (data, time, offset) => {
   const l = data.length;
   const p = data[l - 1];
 
@@ -28,18 +28,26 @@ const sort = (data, time) => {
     d3.select(`#rect${curr[i]}`)
       .transition()
       .duration(500)
-      .attr("x", (20 + 5) * i);
+      .attr("x", (20 + 5) * (i + offset));
+
+    if (i + offset + 1 === curr[i]) {
+      d3.timeout(function () {
+        d3.select(`#rect${curr[i]}`).attr("fill", "blue");
+      }, 500);
+    }
   }
 
   if (right.length > 1) {
     d3.timeout(function () {
-      right = sort(right, time++);
+      // 右側的offset 要加上 這次的 povit 的位置的後一個
+      right = sort(right, time++, curr.indexOf(p) + offset + 1);
     }, 500 * time);
   }
 
   if (left.length > 1) {
     d3.timeout(function () {
-      left = sort(left, time++);
+      // 左側的offset 保留本次的  offset
+      left = sort(left, time++, offset);
     }, 500);
   }
 
